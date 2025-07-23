2d98fac69d6f0fded0c8e5f64583a15238c86a55
Anglebase <1628818324@qq.com>
2025-07-23 20:09:36 +0800

    实现构建命令

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,59 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
+
+function build_project() {
+	console.log('build project');
+	const workspace = vscode.workspace.workspaceFolders;
+	if (!workspace) {
+		// 必须首先开启一个工作区
+		vscode.window.showErrorMessage('No workspace opened');
+		return;
+	}
+	if (workspace.length > 1) {
+		// 多工作区下不自动构建
+		return;
+	}
+
+	const folder = workspace[0];
+
+	// 检查配置文件
+	if (!fs.existsSync(path.join(folder.uri.fsPath, "cup.toml"))) {
+		vscode.window.showErrorMessage('Workspace not a cup project.');
+		return;
+	}
+
+	// 获取终端实例
+	let terminal = vscode.window.terminals.find(t => t.name === 'Cup');
+	if (!terminal) {
+		terminal = vscode.window.createTerminal({
+			name: 'Cup',
+			cwd: folder.uri,
+			hideFromUser: true,
+		});
+	}
+
+	// 执行自动构建
+	let msg = vscode.window.setStatusBarMessage('$(sync)Building...');
+	terminal.show();
+	terminal.sendText("cup build");
+	vscode.window.onDidEndTerminalShellExecution((e) => {
+		if (e.terminal.name !== 'Cup') {
+			return;
+		}
+		msg.dispose();
+		if (e.exitCode === 0) {
+			vscode.window.setStatusBarMessage('$(check)Build succeeded.', 5000);
+			// 构建成功则关闭终端
+			e.terminal.dispose();
+		} else {
+			vscode.window.setStatusBarMessage('$(error)Build failed.', 5000);
+		}
+	});
+}
+
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -10,17 +63,11 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "cppcup" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('cppcup.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from cppcup!');
-	});
+	const build_disposable = vscode.commands.registerCommand('cppcup.build', build_project);
+	context.subscriptions.push(build_disposable);
 
-	context.subscriptions.push(disposable);
+	build_project();
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
